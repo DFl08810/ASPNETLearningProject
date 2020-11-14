9119ebbfd7d0c44e49d277a2666874d9355281f1
bobo <bobo@DESKTOP-9I6AA24>
2020-11-14 16:07:27 +0100

    Added data model

---
+++ DataCore/DataModels/ArticleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataCore.DataModels
+{
+    class ArticleModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public string Content { get; set; }
+    }
+}
