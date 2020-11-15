5ec79fd4f69167af4a7dd4cacde1532be1084d7a
bobo <bobo@DESKTOP-9I6AA24>
2020-11-15 22:27:52 +0100

    First unit test of database wiring mockup, this approach is promising

--- LogicTest/UnitTest1.cs
+++ LogicTest/UnitTest1.cs
@@ -1,3 +1,6 @@
+using CommandCore.Factories;
+using DataCore;
+using DataCore.Entities;
 using System;
 using Xunit;
 
@@ -8,7 +11,14 @@ namespace LogicTest
         [Fact]
         public void Test1()
         {
+            var article = new ArticleDB();
+            var factory = new ArticleFactory(article);
 
+            var x = factory.CreateArticles();
+
+            var g = x[0];
+
+            Assert.Equal("Zivot savcu", g.Title);
         }
     }
 }
