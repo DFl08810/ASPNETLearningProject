e83bf3a46a24ea6596fa4cae9326965970943074
bobo <bobo@DESKTOP-9I6AA24>
2020-11-15 21:17:00 +0100

    project retargeted fot .NET 5.0 from .NET core 3.1

---
+++ DataCore/DataModels/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataCore.DataModels
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+
+
+    }
+}
