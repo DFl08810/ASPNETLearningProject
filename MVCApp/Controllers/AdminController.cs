fcf245a263cea18ae454e613e065c699d51111d6
bobo <bobo@DESKTOP-9I6AA24>
2020-11-22 16:19:38 +0100

    some progress with admin menu

--- MVCApp/Controllers/AdminController.cs
+++ MVCApp/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityLib.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVCApp.EntityServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,29 @@ using System.Threading.Tasks;
 
 namespace MVCApp.Controllers
 {
+    [Authorize(Roles = RoleDef.Admin)]
     public class AdminController : Controller
     {
+        #region fields
+        private IArticleService _articleService;
+        #endregion
+        #region ctor
+        public AdminController(IArticleService articleService)
+        {
+            this._articleService = articleService;
+        }
+        #endregion
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Articles()
+        {
+            var articles = _articleService.FetchArticles();
+            return View(articles);
+        }
+
     }
 }
