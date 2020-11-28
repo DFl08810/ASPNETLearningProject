30c7075e7d3d13770d6e48ef286c6a4751f44633
bobo <bobo@DESKTOP-9I6AA24>
2020-11-28 12:58:39 +0100

    administration accounts panel finished, search button sometimes returns none but on second search it works 1 in  10

--- MVCApp/wwwroot/ts/Control.ts
+++ MVCApp/wwwroot/ts/Control.ts
@@ -11,6 +11,86 @@ $(function () {
     });
 
 });
+
+//selection matrix class
+class SelectionOrigin {
+    static roleSelection: string = "All";
+    static statusSelection: string = "All";
+}
+
+//this sorts table content based on  role selection
+function SortRoles(role) {
+    //iterate rows
+    $("tr.accounts").each(function (index, tr) {
+        //get val attribute from role column
+        var roleValue = $("td.role", tr).attr('val');
+        //get status for checking
+        var statusValue = $("td.status i.table-symbol", tr).attr('val');
+        //var y = $("td.status i.table-symbol", tr).attr('val');
+        console.log(roleValue);
+        //console.log(y);
+        //show or hide rows based on selection
+        if (role == roleValue && statusValue == SelectionOrigin.statusSelection) {
+            $(tr).show();
+        }
+        else if (role != roleValue && role != 'All') {
+            $(tr).hide();
+        }
+        else if (statusValue == SelectionOrigin.statusSelection || SelectionOrigin.statusSelection == 'All') {
+            $(tr).show();
+        }
+        console.log(index);
+        console.log(tr);
+    });
+}
+
+//this sorts table content based on  role selection
+function SortStatus(status) {
+    //iterate rows
+    $("tr.accounts").each(function (index, tr) {
+        //get status value from status column
+        var statusValue = $("td.status i.table-symbol", tr).attr('val');
+        //get role for checking
+        var roleValue = $("td.role", tr).attr('val');
+
+        //console.log(y);
+        //show or hide rows based on selection
+        console.log('deb');
+        console.log(SelectionOrigin.roleSelection);
+        if (status == statusValue && roleValue == SelectionOrigin.roleSelection) {
+            $(tr).show();
+        }
+        else if (status != statusValue && status != 'All') {
+            $(tr).hide();
+        }
+        else if (roleValue == SelectionOrigin.roleSelection || SelectionOrigin.roleSelection == 'All') {
+            $(tr).show();
+        }
+
+        console.log(index);
+        console.log(tr);
+    });
+}
+
+
+//gets selected value from role dropdown and resolve display rows
+$(function () {
+    $(".role-dropdown a").click(function (e) {
+        var role = $(this).attr('val');
+        SelectionOrigin.roleSelection = role;
+        SortRoles(role);
+    });
+
+});
+//gets selected value from status dropdown and resolve display rows
+$(function () {
+    $(".status-dropdown a").click(function (e) {
+        var status = $(this).attr('val');
+        SelectionOrigin.statusSelection = status;
+        SortStatus(status);
+    });
+});
+
 //gets selected value from sort dropdown and calls semi abstracted ajax func 
 $(function () {
     $(".sort-dropdown a").click(function (e) {
@@ -33,6 +113,8 @@ function CallTarget(dataObject, target: string) {
             $('.table-card').empty();
             //append partial view
             $('.table-card').append(data);
+            SortStatus(SelectionOrigin.statusSelection);
+            SortRoles(SelectionOrigin.roleSelection);
         },
         error: function (jqXhr, textStatus, errorMessage) {
             $('p').append('Error' + errorMessage);
@@ -57,6 +139,8 @@ $("#SearchButton").click(function (event) {
         success: function (data, status, xhr) {
             $('.table-card').empty();
             $('.table-card').append(data);
+            SortStatus(SelectionOrigin.statusSelection);
+            SortRoles(SelectionOrigin.roleSelection);
         },
         error: function (jqXhr, textStatus, errorMessage) {
             $('p').append('Error' + errorMessage);
