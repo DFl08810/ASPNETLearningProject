bfcba1d0194da79f1a73a1e9801aca35f4f24f52
bobo <bobo@DESKTOP-9I6AA24>
2020-12-08 20:43:11 +0100

    added component prerequisites

---
+++ MVCApp/ViewComponents/AccountValidationViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.ViewComponents
+{
+    //Component for validating accounts
+    public class AccountValidationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
